499d5576c05dbb17ff8b25b13b8052bb2fa9e9db
JerryBAndersen <jerrybandersen@yahoo.de>
2015-12-09 10:50:32 +0100

    init

---
+++ Assets/Blink.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour {
+
+    public bool raycast = true;
+    public GameObject representation;
+    public float speed = .05f;
+
+    void Start () {
+        representation = Instantiate(representation);
+	}
+	
+	void FixedUpdate () {
+
+        representation.transform.position += speed *Input.GetAxis("Vertical") * representation.transform.forward;
+        representation.transform.position += speed * Input.GetAxis("Horizontal") * representation.transform.right;
+        //representation.transform.Rotate(Quaternion.LookRotation(representation.transform.position-transform.position, Vector3.up).eulerAngles);
+
+        if (Input.GetButtonDown("Fire1")) {
+            transform.position = representation.transform.position;
+            transform.rotation = representation.transform.rotation;
+            representation.transform.position += transform.forward*2f;
+        }
+	}
+}
