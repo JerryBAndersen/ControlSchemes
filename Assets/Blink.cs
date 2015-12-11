1eb173a65f5ec629bb31b39aea56934843bcbf49
JerryBAndersen <jerrybandersen@yahoo.de>
2015-12-11 16:58:29 +0100

    save changes

--- Assets/Blink.cs
+++ Assets/Blink.cs
@@ -3,24 +3,33 @@ using System.Collections;
 
 public class Blink : MonoBehaviour {
 
-    public bool raycast = true;
     public GameObject representation;
-    public float speed = .05f;
+    public bool raycast = false;
+    public GameObject pointer;
+    public float speed = .2f;
+    public float spawnDistance = 3.0f;
 
     void Start () {
         representation = Instantiate(representation);
 	}
 	
 	void FixedUpdate () {
+        if (!raycast)
+        {
+            representation.transform.position += speed * Input.GetAxis("Vertical") * (representation.transform.position - transform.position).normalized;
+            representation.transform.position += speed * Input.GetAxis("Horizontal") * representation.transform.right;
+            representation.transform.rotation = Quaternion.LookRotation(representation.transform.position - transform.position, Vector3.up);
+            if (Input.GetButtonDown("Fire1"))
+            {
 
-        representation.transform.position += speed *Input.GetAxis("Vertical") * representation.transform.forward;
-        representation.transform.position += speed * Input.GetAxis("Horizontal") * representation.transform.right;
-        //representation.transform.Rotate(Quaternion.LookRotation(representation.transform.position-transform.position, Vector3.up).eulerAngles);
-
-        if (Input.GetButtonDown("Fire1")) {
-            transform.position = representation.transform.position;
-            transform.rotation = representation.transform.rotation;
-            representation.transform.position += transform.forward*2f;
+                transform.position = representation.transform.position;
+                transform.rotation = representation.transform.rotation;
+                representation.transform.position += transform.forward * spawnDistance;
+            }
         }
+        else { 
+            
+        }
+
 	}
 }
