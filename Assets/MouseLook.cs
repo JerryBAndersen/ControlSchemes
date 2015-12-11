fcd26c50b96da2805b761a0ce25bc7282b290eda
JerryBAndersen <jerrybandersen@yahoo.de>
2015-12-11 17:20:30 +0100

    improved teleport rotation

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class MouseLook : MonoBehaviour {
+
+    private Vector3 mousePrevious;
+
+    void Update()
+    {
+        //Mouse look if VR is disabled
+        if (!VRDevice.isPresent)
+        {
+            Vector3 mouseCurrent = Input.mousePosition;
+            Vector3 mouseDelta = mouseCurrent - mousePrevious;
+            mousePrevious = mouseCurrent;
+            transform.RotateAround(transform.position, Vector3.up, mouseDelta.x);
+            transform.RotateAround(transform.position, transform.right, -mouseDelta.y);
+        }
+    }
+}
